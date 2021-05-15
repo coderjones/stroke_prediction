fd329a07512cb942d84ba889290fe9909b176b22
coderjones <coderjones@fastmail.com>
2021-05-15 16:27:13 -0500

    Init project

---
+++ stroke_prediction.R
@@ -0,0 +1,10 @@
+# Stroke prediction in R
+library(tidyverse)
+
+
+# set working directory
+setwd("/Users/jeremiahhamilton/code/stroke_prediction")
+
+# read in data
+df <- read.csv("healthcare-dataset-stroke-data.csv")
+
